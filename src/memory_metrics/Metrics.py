095266120aba974d9e37e09d4b3f428e4a800833
rynkuns <szymon.rynkun@gmail.com>
2024-04-11 20:23:11 +0200

    tqdm

--- src/memory_metrics/Metrics.py
+++ src/memory_metrics/Metrics.py
@@ -1,6 +1,23 @@
 from openai import OpenAI
 import numpy as np
-from tqdm.notebook import tqdm
+# from tqdm.notebook import tqdm
+
+def isnotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
+
+if isnotebook():
+    from tqdm.notebook import tqdm
+else:
+    from tqdm import tqdm
 
 
 class Metrics():
