09ea491b0cc8de570fcfb23bb233381e4eb699fc
rynkuns <szymon.rynkun@gmail.com>
2024-04-11 22:03:58 +0200

    tqdm

--- src/memory_metrics/Metrics.py
+++ src/memory_metrics/Metrics.py
@@ -6,8 +6,11 @@ def isnotebook():
     # https://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook#39662359
     try:
         shell = get_ipython().__class__.__name__
+        print(shell)
         if shell == 'ZMQInteractiveShell':
             return True   # Jupyter notebook or qtconsole
+        elif shell == "google.colab._shell":
+            return True
         elif shell == 'TerminalInteractiveShell':
             return False  # Terminal running IPython
         else:
