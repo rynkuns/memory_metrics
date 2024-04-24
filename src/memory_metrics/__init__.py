3ba71a3791dc85b77afdddeaff8ce6afe4101e9d
rynkuns <szymon.rynkun@gmail.com>
2024-04-24 15:19:31 +0200

    init fixes

--- src/memory_metrics/__init__.py
+++ src/memory_metrics/__init__.py
@@ -3,5 +3,5 @@ from memory_metrics.metrics import Metrics
 import importlib.metadata
 
 __version__ = importlib.metadata.version("memory_metrics")
-__name__ = importlib.metadata.__name__
+# __name__ = importlib.metadata.__name__
 # __name__ = "memory_metrics"
